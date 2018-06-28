74f08a09d429d54aff343bf081a83761037839c2
Shalitha Suranga <shalithasuranga@gmail.com>
2018-06-28 12:49:40 +0530

    Add auth to build

--- core-windows/build.bat
+++ core-windows/build.bat
@@ -3,7 +3,7 @@ echo Neutralino is being built...
 if NOT EXIST bin (
     mkdir bin
 )
-g++ src/main.cpp src/settings.cpp src/requestparser.cpp src/serverlistener.cpp src/functions.cpp src/core/computer.cpp src/core/filesystem.cpp src/core/os.cpp src/router.cpp -lws2_32 -static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic -o bin/neutralino 
+g++ src/main.cpp src/settings.cpp src/requestparser.cpp src/serverlistener.cpp src/functions.cpp src/core/computer.cpp src/core/filesystem.cpp src/core/os.cpp src/router.cpp src/auth/authbasic.cpp -lws2_32 -static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic -o bin/neutralino 
 
 echo Neutralino binary is compiled in to bin/netralino.exe
 
