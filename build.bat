597eb863664c12d0090aa27e92b7281730624e4a
Shalitha Suranga <shalithasuranga@gmail.com>
2018-07-12 11:42:27 +0530

    Create the main batch file

---
+++ build.bat
@@ -0,0 +1,35 @@
+@echo off
+
+REM MIT License
+
+REM Copyright (c) 2018 Neutralinojs
+
+REM Permission is hereby granted, free of charge, to any person obtaining a copy
+REM of this software and associated documentation files (the "Software"), to deal
+REM in the Software without restriction, including without limitation the rights
+REM to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+REM copies of the Software, and to permit persons to whom the Software is
+REM furnished to do so, subject to the following conditions:
+
+REM The above copyright notice and this permission notice shall be included in all
+REM copies or substantial portions of the Software.
+
+REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+REM IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+REM FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+REM AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+REM LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+REM OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+REM SOFTWARE.
+
+cd core-windows
+cmd /c build.bat
+echo Building neutralino.js
+cd ..\neutralino.js
+cmd /c npm run build
+xcopy /y dist\neutralino.js ..\core-windows\bin\app\assets\neutralino.js
+cd ..
+xcopy /s /y core-windows\bin dist
+
+
+
