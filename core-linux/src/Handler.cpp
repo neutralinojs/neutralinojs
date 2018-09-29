5d911651fd0f6fe696d4095140cb99ee3001cbcc
Shalitha <shalithasuranga@gmail.com>
2018-09-29 10:23:43 +0530

    Use std threads - major change

--- core-linux/src/Handler.cpp
+++ core-linux/src/Handler.cpp
@@ -25,56 +25,28 @@
 
 using namespace std;
 
-Handler::Handler(const int connfd)
-      :_connfd(connfd)
-{
-    _isClosed = false;
-}
+void Handler::handle(int _connfd)
 
-Handler::~Handler()
-{
-    if(!_isClosed)
-        close(_connfd);
-}
 
-void Handler::handle()
 {
-    if(!receiveRequest())
-    {
-        close(_connfd);
-        _isClosed = true;
+
+    Buffer _inputBuffer;
+    Buffer _outputBuffer;
+    HTTPRequest _request;
+
+    if(_inputBuffer.readFd(_connfd) == 0){
+        close(_connfd);      
         return;
     }
+    std::string request = _inputBuffer.readAllAsString();
+    Parser p(request);
+    _request = p.getParseResult();
+
     pair<string, string> resp = routes::handle(_request.uri, _request.body, _request.auth);
+    std::cout << "url = " << _request.uri << " id = " << _connfd << std::endl;
     string content =  resp.first;
     std::string msg = "HTTP/1.1 200 OK\r\nContent-Type:" + resp.second + "\r\nContent-Length: " + std::to_string(content.size()) +"\r\nConnection: close\r\n\r\n" + content;
     _outputBuffer.append(msg.c_str(), msg.size());
     _outputBuffer.sendFd(_connfd);
     close(_connfd);
-    _isClosed = true;
-}
-
-bool Handler::receiveRequest()
-{ 
-    if(_inputBuffer.readFd(_connfd) == 0)
-        return false;
-    std::string request = _inputBuffer.readAllAsString();
-    Parser p(request);
-    _request = p.getParseResult();
-    return true;
-}
-
-void Handler::sendErrorMsg()
-{
-
-}
-
-void Handler::parseURI()
-{
-
-}
-
-void Handler::getFileType()
-{
-
 }
