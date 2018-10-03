814cbe192000239a17e98b8905c389cf41518244
Tharindu Malshan <36675143+tharindumalshan1@users.noreply.github.com>
2018-10-03 12:02:33 +0530

    Update webpack.config.js

--- neutralino.js/webpack.config.js
+++ neutralino.js/webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
   entry: {
             index : './src/index.js'
         },
+  console.log(path);
+  
   output: {
     filename: 'neutralino.js',
     path: path.resolve(__dirname, 'dist'),
