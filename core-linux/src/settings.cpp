86c5853458c13ffc9ef25f9fae7ba1666dbbe7d5
Shalitha Suranga <shalithasuranga@gmail.com>
2018-12-12 02:10:18 -0800

    Add NL_VERSION global variable

--- core-linux/src/settings.cpp
+++ core-linux/src/settings.cpp
@@ -63,6 +63,7 @@ namespace settings {
     string getGlobalVars(){
         json settings = getOptions();
         string s = "var NL_OS='Linux';";
+        s += "var NL_VERSION='1.0.8';";
         s += "var NL_NAME='" + settings["appname"].get<std::string>() + "';"; 
         s += "var NL_PORT=" + settings["appport"].get<std::string>() + ";";
         s += "var NL_MODE='" + settings["mode"].get<std::string>() + "';";
