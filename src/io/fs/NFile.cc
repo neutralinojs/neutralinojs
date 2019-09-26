4a59f4bbff16ac31f636e84b89cb47fc7bf37f73
Kasun Vithanage <alankasun@gmail.com>
2019-09-26 10:30:25 +0530

    clang format

--- src/io/fs/NFile.cc
+++ src/io/fs/NFile.cc
@@ -10,51 +10,39 @@
 using namespace std;
 using namespace neut;
 
-NFile::NFile(const string path)
-    : path(path)
-{
-    exists = filesystem::exists(path);
-    dir = filesystem::is_directory(path);
+NFile::NFile(const string path) : path(path) {
+  exists = filesystem::exists(path);
+  dir = filesystem::is_directory(path);
 }
 
-void NFile::read(ios_base::openmode mode)
-{
-    ifstream ifs;
-    ios_base::openmode _openmode = ios_base::ate | mode; // always seek to the end
+void NFile::read(ios_base::openmode mode) {
+  ifstream ifs;
+  ios_base::openmode _openmode = ios_base::ate | mode; // always seek to the end
 
-    ifs.open(path, _openmode);
-    if (ifs.fail()) {
-        throw runtime_error(strerror(errno));
-    }
+  ifs.open(path, _openmode);
+  if (ifs.fail()) {
+    throw runtime_error(strerror(errno));
+  }
 
-    ifstream::pos_type pos = ifs.tellg();
-    vector<char> tmp(pos);
+  ifstream::pos_type pos = ifs.tellg();
+  vector<char> tmp(pos);
 
-    ifs.seekg(0, ios::beg);
-    ifs.read(&tmp[0], pos); // read pos bytes from the stream into the vector
+  ifs.seekg(0, ios::beg);
+  ifs.read(&tmp[0], pos); // read pos bytes from the stream into the vector
 
-    data = tmp;
+  data = tmp;
 }
 
-void NFile::write(vector<char> data, ios_base::openmode mode)
-{
-    ofstream output(path, mode);
-    if (output.fail()) {
-        throw runtime_error(strerror(errno));
-    }
+void NFile::write(vector<char> data, ios_base::openmode mode) {
+  ofstream output(path, mode);
+  if (output.fail()) {
+    throw runtime_error(strerror(errno));
+  }
 
-    // write data to the disk
-    output.write(data.data(), data.size());
+  // write data to the disk
+  output.write(data.data(), data.size());
 }
 
-vector<char>
-NFile::getBytes()
-{
-    return data;
-}
+vector<char> NFile::getBytes() { return data; }
 
-string
-NFile::getString()
-{
-    return string(data.begin(), data.end());
-}
+string NFile::getString() { return string(data.begin(), data.end()); }
