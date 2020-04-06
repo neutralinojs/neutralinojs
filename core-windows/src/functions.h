8ed496def04a168583260358f7a36edfab381fea
Aina Rasoldier <aina.rasoldier@inria.fr>
2020-04-07 01:54:54 +0200

    fix url encoding in routes

--- core-windows/src/functions.h
+++ core-windows/src/functions.h
@@ -31,6 +31,7 @@ using namespace std;
 namespace functions {
     vector<string> split(const string &s, char delim);
     string generateToken(int len = 32);
+    string decodeUrl(string &encoded);
 }
 
 #endif // end of FUNCTIONS_H
