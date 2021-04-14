a6e9b9aa29d78a2f175700502fa80138127487fa
Shalitha Suranga <shalithasuranga@gmail.com>
2021-04-14 04:53:18 -0700

    Implement storage modifications on Windows

--- core-windows/src/api/storage/storage.cpp
+++ core-windows/src/api/storage/storage.cpp
@@ -25,8 +25,10 @@
 #include <windows.h>
 #include "lib/json.hpp"
 #include "settings.h"
+#include "../filesystem/filesystem.h"
 
 #define STORAGE_DIR "/.storage"
+#define STORAGE_EXT ".neustorage"
 
 using namespace std;
 using json = nlohmann::json;
@@ -34,9 +36,9 @@ using json = nlohmann::json;
 namespace storage {
     string getData(json input) {
         json output;
-        string bucket = input["bucket"].get<std::string>();
+        string bucket = input["bucket"];
         string bucketPath = settings::joinAppPath(STORAGE_DIR);
-        string filename = bucketPath + "/" + bucket + ".json";
+        string filename = bucketPath + "/" + bucket + STORAGE_EXT;
         ifstream t;
         t.open(filename);
         if(!t.is_open()) {
@@ -57,18 +59,25 @@ namespace storage {
 
     string putData(json input) {
         json output;
-        string bucket = input["bucket"].get<std::string>();
+        string bucket = input["bucket"];
         string bucketPath = settings::joinAppPath(STORAGE_DIR);
         CreateDirectory(bucketPath.c_str(), NULL);
-        string filename = bucketPath + "/" + bucket + ".json";
-        string content = input["data"];
-        ofstream t(filename);
-        if(!t.is_open()) {
-            output["error"] = "Unable to write storage bucket: " + bucket;
-            return output.dump();
+        string filename = bucketPath + "/" + bucket + STORAGE_EXT;
+        if(input["data"].is_null()) {
+            json readFileParams;
+            readFileParams["fileName"] = filename;
+            fs::removeFile(readFileParams);
         }
-        t << content;
-        t.close();
+        else {
+            string content = input["data"];
+            ofstream t(filename);
+            if(!t.is_open()) {
+                output["error"] = "Unable to write storage bucket: " + bucket;
+                return output.dump();
+            }
+            t << content;
+            t.close();
+        }
         output["success"] = true;
         output["message"] = "Wrote data to " + bucket;
         return output.dump();
