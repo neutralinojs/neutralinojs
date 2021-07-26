27fff8e1353955d83bc4f201abdaf2339a56093a
Shalitha Suranga <shalithasuranga@gmail.com>
2021-07-26 22:34:00 +0530

    #532: Add support to build 32-bit binaries on Linux

--- build_linux.sh
+++ build_linux.sh
@@ -1,9 +1,28 @@
-echo "Compiling Neutralinojs..."
-if [ -e bin/neutralino-linux ]; then
-    rm bin/neutralino-linux
+ARCH=$1
+# Trigger x64 build by default
+if [ "$ARCH" == "" ]; then
+    ARCH="x64"
 fi
 
-g++ resources.cpp \
+NEU_BIN="neutralino-linux_$ARCH"
+
+if [ "$ARCH" == "ia32" ]; then
+    export PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig
+    FLAGS="-m32"
+elif [ "$ARCH" == "x64" ]; then
+    export PKG_CONFIG_PATH=/usr/lib/pkgconfig
+else
+    echo "Unsupported instruction set architecture: $ARCH"
+    exit 1
+fi
+
+if [ -e bin/$NEU_BIN ]; then
+    rm bin/$NEU_BIN
+fi
+
+echo "Compiling Neutralinojs $ARCH..."
+
+g++ $FLAGS resources.cpp \
     helpers.cpp \
     main.cpp \
     server/router.cpp \
@@ -26,13 +45,14 @@ g++ resources.cpp \
     -DELPP_NO_DEFAULT_LOG_FILE=1 \
     -DWEBVIEW_GTK=1 \
     `pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0 glib-2.0 appindicator3-0.1` \
-    -o bin/neutralino-linux \
+    -o bin/$NEU_BIN \
     -no-pie \
     -Os \
      -I .
 
-if [ -e bin/neutralino-linux ]; then
-    echo "OK: Neutralino binary is compiled in to bin/neutralino-linux"
+if [ -e bin/$NEU_BIN ]; then
+    echo "OK: Neutralino binary is compiled in to bin/$NEU_BIN"
 else
     echo "ERR: Neutralino binary is not compiled"
+    exit 1
 fi
