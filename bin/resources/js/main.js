56270b6a7d2775afdc60d6520c625f977aee2a30
Shalitha Suranga <shalithasuranga@gmail.com>
2021-11-04 18:42:36 +0530

    Update sameple app resources

--- bin/resources/js/main.js
+++ bin/resources/js/main.js
@@ -1,18 +1,18 @@
+function showInfo() {
+    document.getElementById('info').innerHTML = `${NL_APPID} is running on port ${NL_PORT}
+        inside ${NL_OS}<br/><br/><span>v${NL_VERSION}</span>`;
+}
 
-window.myApp = {
-    showInfo: () => {
-        document.getElementById('info').innerHTML = NL_APPID + " is running on port " +
-                    NL_PORT + " inside " + NL_OS + ".<br/><br/>" + "<span>v" + NL_VERSION + "</span>";
-    },
-    openDocs: () => {
-        Neutralino.app.open({
-            url: "https://neutralino.js.org/docs"
-        });
-    }
-};
+function openDocs() {
+    Neutralino.app.open("https://neutralino.js.org/docs");
+}
 
 Neutralino.init();
+Neutralino.events.on("ready", () => {
+    // Immediate native calls
+});
 Neutralino.events.on("windowClose", () => {
     Neutralino.app.exit();
 });
-window.myApp.showInfo();
+
+showInfo();
