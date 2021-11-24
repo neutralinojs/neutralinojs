97ddb247c06823df1e35e814ea7d31d85873e672
Shalitha Suranga <shalithasuranga@gmail.com>
2021-11-24 20:32:50 +0530

    Add tests for app.broadcast

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -44,4 +44,41 @@ describe('app.spec: app namespace tests', () => {
             assert.ok(typeof JSON.parse(runner.getOutput()) == 'object');
         });
     });
+
+    describe('app.broadcast', () => {
+        it('triggers the registered event callback', async () => {
+            let exitCode = runner.run(`
+                function onTestEvent(evt) {
+                    __close('done');
+                }
+                await Neutralino.events.on('testEvent', onTestEvent);
+                await Neutralino.app.broadcast('testEvent');
+            `);
+            assert.equal(runner.getOutput(), 'done');
+        });
+
+        it('triggers the registered event callback with data', async () => {
+            let exitCode = runner.run(`
+                function onTestEvent(evt) {
+                    __close(evt.detail);
+                }
+                await Neutralino.events.on('testEvent', onTestEvent);
+                await Neutralino.app.broadcast('testEvent', 'data');
+            `);
+            assert.equal(runner.getOutput(), 'data');
+        });
+
+        it('throws an error for missing params', async () => {
+            let exitCode = runner.run(`
+                try {
+                    await Neutralino.app.broadcast();
+                }
+                catch(err) {
+                    await __close(err.code);
+                }
+            `);
+            assert.equal(runner.getOutput(), 'NE_RT_NATRTER');
+        });
+    });
+
 });
