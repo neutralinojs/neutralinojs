011571d28c793e375601ba4eadb31468e83223ec
Shalitha Suranga <shalithasuranga@gmail.com>
2021-12-08 19:46:41 +0530

    Code style updates to spec

--- spec/updater.spec.js
+++ spec/updater.spec.js
@@ -20,7 +20,7 @@ describe('updater.spec: updater namespace tests', () => {
         it('works with parameters', async () => {
             let exitCode = runner.run(`
                 let info = await Neutralino.updater
-                    .checkForUpdates('http://localhost:8080/updater_test/update_info.json')
+                    .checkForUpdates('http://localhost:8080/updater_test/update_info.json');
 
                 await __close(JSON.stringify(info));
             `, { args: '--port=8080' });
@@ -48,7 +48,7 @@ describe('updater.spec: updater namespace tests', () => {
         it('replaces res.neu with resourcesURL data', async () => {
             let exitCode = runner.run(`
                 await Neutralino.updater
-                    .checkForUpdates('http://localhost:8080/updater_test/update_info.json')
+                    .checkForUpdates('http://localhost:8080/updater_test/update_info.json');
 
                 //Try deleting existing res.neu just for testing
                 try {
