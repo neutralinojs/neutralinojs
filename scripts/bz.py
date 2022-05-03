d590481a21080de4f7974f76f98065a9140069a5
Shalitha Suranga <shalithasuranga@gmail.com>
2022-05-03 23:53:55 +0530

    Update bz.py

--- scripts/bz.py
+++ scripts/bz.py
@@ -36,7 +36,7 @@ def get_compiler():
     return compilers[BZ_OS] + ' '
 
 def get_latest_commit():
-    return subprocess.getoutput('git log -n 1 origin/main --pretty=format:"%H"') \
+    return subprocess.getoutput('git rev-parse HEAD') \
             .strip()
 
 def apply_template_vars(text):
