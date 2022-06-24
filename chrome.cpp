9edc0589e4d37a55522156e5eb3aacd197283798
Sergei Golimbievsky <sergei@gitwit.com>
2022-06-24 10:16:23 -0500

    Remove --enable-automation Chrome flag to fix #915. (#938)

--- chrome.cpp
+++ chrome.cpp
@@ -41,7 +41,6 @@ string __getDefaultChromeArgs() {
     "--no-first-run "
     "--no-default-browser-check "
     "--safebrowsing-disable-auto-update "
-    "--enable-automation "
     "--password-store=basic "
     "--use-mock-keychain";
 }
