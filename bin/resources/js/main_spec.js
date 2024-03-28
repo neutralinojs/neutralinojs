e357a7008c34075de73ae751bae3a16670e31886
Sadaf-A <sadaf20043006@gmail.com>
2024-03-28 19:34:50 +0530

    added: functionality to copy file when destination file already exists

---
+++ bin/resources/js/main_spec.js
@@ -0,0 +1,34 @@
+
+
+
+Neutralino.init();
+
+Neutralino.events.on("ready", async () => {
+    await __init();
+    
+                await Neutralino.filesystem.createDirectory(NL_PATH + '/.tmp/abc');
+                await __close('done');
+            
+});
+
+async function __close(data = "", exitCode = 0) {
+    if(data) {
+        await Neutralino.filesystem.writeFile(NL_PATH + "/.tmp/output.txt", data);
+    }
+    setTimeout(async () => {
+        await Neutralino.app.exit(exitCode); // normal exit
+    }, 2000);
+}
+
+async function __init() {
+    try {
+        await Neutralino.filesystem.createDirectory(NL_PATH + "/.tmp");
+    }
+    catch(err) {
+        // ignore
+    }
+    setTimeout(async () => {
+        await Neutralino.filesystem.writeFile(NL_PATH + "/.tmp/output.txt", 'NL_SP_MAXTIMT');
+        await Neutralino.app.exit(1); // max timeout force exit
+    }, 20000);
+}
