cf0d4fa846bced16854f849a3a1fbdd15147cc45
Shalitha Suranga <shalithasuranga@gmail.com>
2024-04-05 12:23:11 +0530

    Update helpers.cpp

--- helpers.cpp
+++ helpers.cpp
@@ -110,7 +110,12 @@ vector<string> getModes() {
 
 string normalizePath(string &path) {
     #if defined(_WIN32)
-    replace(path.begin(), path.end(), '\\', '/');
+    if(path.find('\\') != string::npos) {
+        replace(path.begin(), path.end(), '\\', '/');
+    {
+    else {
+        replace(path.begin(), path.end(), '/', '\\');
+    }
     #endif
     return path;
 }
