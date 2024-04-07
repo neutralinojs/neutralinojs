2c49c130330abebe27194ea631ba254f081518d3
moataz2002 <mezzo.m.2030@gmail.com>
2024-04-07 16:07:50 +0200

    update chrome.h documentation

--- chrome.h
+++ chrome.h
@@ -1,3 +1,12 @@
+/**
+ * @file chrome.h
+ * @brief Header file containing declarations for the chrome namespace.
+ * 
+ * This header file declares a function for initializing Chrome-related functionality
+ * within a WebSocket server application. It provides a namespace `chrome` to encapsulate 
+ * these functionalities.
+ */
+
 #ifndef NEU_CHROME_H
 #define NEU_CHROME_H
 
@@ -11,9 +20,16 @@ using json = nlohmann::json;
 
 namespace chrome {
 
+/**
+ * @brief Initializes Chrome-related functionality with the provided input.
+ * 
+ * This function is responsible for initializing Chrome-related functionality
+ * within the WebSocket server application based on the provided input parameters.
+ * 
+ * @param input A JSON object containing input parameters for initialization.
+ */
 void init(const json &input);
 
-} // namesapce chrome
+} // namespace chrome
 
-#endif // #define NEU_CHROME_H
-
+#endif // NEU_CHROME_H
