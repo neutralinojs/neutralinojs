a4f59d1b404cc57c6bf31f347c33a17f9e434a17
Sadaf-A <sadaf20043006@gmail.com>
2024-05-24 19:21:14 +0530

    added: additional testcases for debug API

--- spec/debug.spec.js
+++ spec/debug.spec.js
@@ -5,7 +5,7 @@ const runner = require('./runner');
 describe('debug.spec: debug namespace tests', () => {
     describe('debug.log', () => {
         it('logs a simple message', async () => {
-            let exitCode = runner.run(`
+            runner.run(`
                 await Neutralino.debug.log('Hello! This is a simple message');
                 let logFileContent = await Neutralino.filesystem.readFile(NL_PATH + '/neutralinojs.log');
                 await __close(logFileContent);
@@ -13,13 +13,58 @@ describe('debug.spec: debug namespace tests', () => {
             assert.ok(runner.getOutput().includes('Hello! This is a simple message'));
         });
 
+        it('logs an info message', async () => {
+            runner.run(`
+                await Neutralino.debug.log('Hello! This is a info message', 'INFO');
+                let logFileContent = await Neutralino.filesystem.readFile(NL_PATH + '/neutralinojs.log');
+                await __close(logFileContent);
+            `);
+            assert.ok(runner.getOutput().includes('Hello! This is a info message'));
+        });
+
         it('logs an error message', async () => {
-            let exitCode = runner.run(`
+            runner.run(`
                 await Neutralino.debug.log('Hello! This is an error message', 'ERROR');
                 let logFileContent = await Neutralino.filesystem.readFile(NL_PATH + '/neutralinojs.log');
                 await __close(logFileContent);
             `);
             assert.ok(runner.getOutput().includes('Hello! This is an error message'));
+        });
+
+        it('logs a warning message', async () => {
+            runner.run(`
+                await Neutralino.debug.log('Hello! This is a warning message', 'WARNING');
+                let logFileContent = await Neutralino.filesystem.readFile(NL_PATH + '/neutralinojs.log');
+                await __close(logFileContent);
+            `);
+            assert.ok(runner.getOutput().includes('Hello! This is a warning message'));
+        });
+
+        it('logs a warning message', async () => {
+            runner.run(`
+                await Neutralino.debug.log('Hello! This is a debug message', 'DEBUG');
+                let logFileContent = await Neutralino.filesystem.readFile(NL_PATH + '/neutralinojs.log');
+                await __close(logFileContent);
+            `);
+            assert.ok(runner.getOutput().includes('Hello! This is a debug message'));
+        });
+
+        it('logs a message with special characters', async () => {
+            runner.run(`
+                await Neutralino.debug.log('Special characters: @#$%^&*☁☀☊☄');
+                let logFileContent = await Neutralino.filesystem.readFile(NL_PATH + '/neutralinojs.log');
+                await __close(logFileContent);
+            `);
+            assert.ok(runner.getOutput().includes('Special characters: @#$%^&*☁☀☊☄'));
+        });
+
+        it('handles empty log messages gracefully', async () => {
+            runner.run(`
+                await Neutralino.debug.log('');
+                let logFileContent = await Neutralino.filesystem.readFile(NL_PATH + '/neutralinojs.log');
+                await __close(logFileContent);
+            `);
+            assert.ok(runner.getOutput().includes(''));
         });
     });
 });
