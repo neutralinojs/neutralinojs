ed2ef4a016bf024c7f28a0686979896f02d9750d
Shalitha Suranga <shalithasuranga@gmail.com>
2024-07-15 19:35:07 +0530

    Placing test cases properly within namespace

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -2,26 +2,25 @@ const assert = require('assert');
 const runner = require('./runner');
 
 describe('app.spec: app namespace tests', () => {
+        describe('app.exit', () => {
+                it('works without parameters', async () => {
+            let exitCode = runner.run(`
+                setTimeout(() => {
+                    Neutralino.app.exit();
+                }, 2000);
+            `);
+            assert.ok(typeof exitCode != undefined);
+        });
 
-    it('works without parameters', async () => {
-        let exitCode = runner.run(`
-            setTimeout(() => {
-                Neutralino.app.exit();
-            }, 2000);
-        `);
-        assert.ok(typeof exitCode != undefined);
-    });
-
-    it('works with parameters', async () => {
-        let exitCode = runner.run(`
-            setTimeout(() => {
-                Neutralino.app.exit(1);
-            }, 2000);
-        `);
-        assert.ok(typeof exitCode != undefined);
-    });
-
-    describe('app.exit', () => {
+        it('works with parameters', async () => {
+            let exitCode = runner.run(`
+                setTimeout(() => {
+                    Neutralino.app.exit(1);
+                }, 2000);
+            `);
+            assert.ok(typeof exitCode != undefined);
+        });
+        
         it('throws an error for invalid exit codes', async () => {
             runner.run(`
                 try {
