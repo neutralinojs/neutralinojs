92561d7016e4d6b95b318deb16f203ee8b83fe86
Shalitha Suranga <shalithasuranga@gmail.com>
2025-01-14 20:11:02 +0530

    Fix spec related issues

---
+++ spec/server.spec.js
@@ -0,0 +1,56 @@
+const assert = require('assert');
+
+const runner = require('./runner');
+
+describe('server.spec: server namespace tests', () => {
+
+    describe('server.mount', () => {
+        it('mounts and serves a directory successfully', async () => {
+            runner.run(`
+                const response = {};
+                const targetPath = NL_PATH + '/.tmp/test-mount';
+                await Neutralino.filesystem.createDirectory(targetPath);
+                await Neutralino.filesystem.writeFile(targetPath + '/test.txt', 'Hello');
+
+                const fetch1 = await fetch('/test/test.txt');
+                response.fetch1 = fetch1.status;
+
+                await Neutralino.server.mount('/test', targetPath);
+
+                const fetch2 = await fetch('/test/test.txt');
+                response.fetch2 = fetch2.status;
+
+                await __close(JSON.stringify(response));
+            `);
+            const output = JSON.parse(runner.getOutput());
+            assert.ok(typeof output === 'object', 'Expected output is an object');
+            assert.ok(output.fetch1 === 404, 'Expected a request to a file in a yet not mounted directory to fail');
+            assert.ok(output.fetch2 === 200, 'Expected a request to a file in a mounted directory to succeed');
+        });
+        it('unmounts a directory successfully', async () => {
+            runner.run(`
+                const response = {};
+                const targetPath = NL_PATH + '/.tmp/test-mount';
+                await Neutralino.filesystem.createDirectory(targetPath);
+                await Neutralino.filesystem.writeFile(targetPath + '/test.txt', 'Hello');
+
+                await Neutralino.server.mount('/test', targetPath);
+
+                const fetch1 = await fetch('/test/test.txt');
+                response.fetch1 = fetch1.status;
+
+                await Neutralino.server.unmount('/test');
+
+                const fetch2 = await fetch('/test/test.txt');
+                response.fetch2 = fetch2.status;
+
+                await __close(JSON.stringify(response));
+            `);
+            const output = JSON.parse(runner.getOutput());
+            assert.ok(typeof output === 'object', 'Expected output is an object');
+            assert.ok(output.fetch1 === 200, 'Expected a file request to a mounted directory before unmounting it to succeed');
+            assert.ok(output.fetch2 === 404, 'Expected a file request to an unmounted directory to fail');
+        });
+    });
+
+});
