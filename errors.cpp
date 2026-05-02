df4cbeb405fe038833706bd5ca48433caccdf86c
Rizky Mirzaviandy Priambodo <142987522+Xavrir@users.noreply.github.com>
2026-05-02 21:00:54 +0700

    fix: correct typo in NE_CO_UNLTOMG error message (#1650)
    Fix double 'n' typo: 'grabbinng' -> 'grabbing'
    
    Closes #1641
    
    Co-authored-by: Shalitha Suranga <shalithasuranga@gmail.com>

--- errors.cpp
+++ errors.cpp
@@ -93,7 +93,7 @@ string __findStatusCodeDesc(errors::StatusCode code) {
         case errors::NE_OS_INVKNPT: return "Invalid platform path name: %1";
         // computer
         case errors::NE_CO_UNLTOSC: return "Unable to set mouse cursor";
-        case errors::NE_CO_UNLTOMG: return "Unable to set mouse grabbinng";
+        case errors::NE_CO_UNLTOMG: return "Unable to set mouse grabbing";
         case errors::NE_CO_UNLTOSK: return "Unable to send keys";
         // extensions
         case errors::NE_EX_EXTNOTC: return "%1 is not connected yet";
