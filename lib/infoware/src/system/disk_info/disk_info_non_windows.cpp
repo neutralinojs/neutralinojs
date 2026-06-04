5dead0680d32c792b403c76fff41c86beb4e3cf8
Prasun Baranwal <prasubaran@gmail.com>
2026-06-04 16:33:40 +0530

    infoware file update

--- lib/infoware/src/system/disk_info/disk_info_non_windows.cpp
+++ lib/infoware/src/system/disk_info/disk_info_non_windows.cpp
@@ -1,17 +1,4 @@
-// infoware - C++ System information Library
-//
-// Written in 2016-2020 by nabijaczleweli <nabijaczleweli@gmail.com> and ThePhD <phdofthehouse@gmail.com>
-//
-// To the extent possible under law, the author(s) have dedicated all copyright and related
-// and neighboring rights to this software to the public domain worldwide. This software is
-// distributed without any warranty.
-//
-// You should have received a copy of the CC0 Public Domain Dedication along with this software.
-// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>
-
-
 #ifndef _WIN32
-
 
 #include "infoware/system.hpp"
 #include <cerrno>
