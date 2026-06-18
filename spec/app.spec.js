b791968ede8c549a67d113ca1767ba498a87b407
Shalitha Suranga <shalithasuranga@gmail.com>
2026-06-18 13:23:29 +0530

    Update app.spec.js

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -45,8 +45,8 @@ describe('app.spec: app namespace tests', () => {
     describe('app.getProcessId', () => {
         it('returns the current process id', async () => {
             runner.run(`
-                let info = await Neutralino.app.getProcessId();
-                await __close(JSON.stringify(info));
+                let pid = await Neutralino.app.getProcessId();
+                await __close(JSON.stringify(pid));
             `);
             const pid = JSON.parse(runner.getOutput());
             assert.ok(typeof pid === 'number');
